32186f948fa23382b4e61d2d12e52403127f9b5d
lindliu <dliu2020@outlook.com>
2022-11-16 14:51:39 +0100

    f

--- train_odeint_1/train_odeint.py
+++ train_odeint_1/train_odeint.py
@@ -158,8 +158,8 @@ if __name__ == '__main__':
     
     batch_t = t
     for itr in range(1, 20000):
-        # idx = np.random.choice(np.arange(data.shape[0]),batch_size)
-        idx = np.array([0,1])
+        idx = np.random.choice(np.arange(data.shape[0]),batch_size)
+        # idx = np.array([0,1])
         batch_y = torch.tensor(data[idx, ...], dtype=torch.float32).to(device)
         batch_y0 = batch_y[:,0,:].to(device)
 
